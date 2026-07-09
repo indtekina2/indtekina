99ab432710cee4456271aaa2209eab7589deda74
indtekina2 <indtekina2@gmail.com>
2026-07-09 14:01:29 +0530

    Completed the about me page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./About.css"
 import { TypeAnimation } from "react-type-animation";
+import resume from "../assets/resume.pdf";
+import { useNavigate } from "react-router-dom";
 
 function About() {
+  const navigate = useNavigate()
   return (
     <div className="about_container">
       <div className="about">
-        <div className="about_text">
+        <div className="about_hero">
           <h2>Hi, I am Indtekina</h2>
           <h3>
-            <span>I <TypeAnimation
+            <span>
+              I{" "}
+              <TypeAnimation
                 sequence={[
                   "build websites...",
                   1000,
@@ -26,6 +31,24 @@ function About() {
               />
             </span>
           </h3>
+          <p>
+            I enjoy building modern, responsive web applications and
+            continuously learning new technologies to become a better full-stack
+            developer.{" "}
+          </p>
+
+          <div className="about_buttons">
+            <a href={resume} download>
+              <button className="full_button">Resume</button>
+            </a>
+
+            <button
+              className="secondary_btn empty_button"
+              onClick={() => navigate("/projects")}
+            >
+              Projects
+            </button>
+          </div>
         </div>
       </div>
     </div>
